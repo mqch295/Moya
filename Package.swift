fffada313fa92a2ec77a99c29ec2c0f20b8de76d
Mqch <mqch295@gmail.com>
2021-06-30 16:00:16 +0800

    spm remove unitTest

--- Package.swift
+++ Package.swift
@@ -16,16 +16,11 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/Alamofire/Alamofire.git", .upToNextMajor(from: "5.0.0")),
-        .package(url: "https://github.com/ReactiveX/RxSwift.git", .upToNextMajor(from: "5.0.0")),
-        .package(url: "https://github.com/Quick/Quick.git", .upToNextMajor(from: "2.0.0")), // dev
-        .package(url: "https://github.com/Quick/Nimble.git", .upToNextMajor(from: "8.0.0")), // dev
-        .package(url: "https://github.com/AliSoftware/OHHTTPStubs.git", .upToNextMajor(from: "9.0.0")), // dev
-        .package(url: "https://github.com/shibapm/Rocket", .upToNextMajor(from: "1.0.0")) // dev
+        .package(url: "https://github.com/ReactiveX/RxSwift.git", .upToNextMajor(from: "5.0.0"))
     ],
     targets: [
         .target(name: "Moya", dependencies: ["Alamofire"]),
-        .target(name: "RxMoya", dependencies: ["Moya", "RxSwift"]),
-        .testTarget(name: "MoyaTests", dependencies: ["Moya", "RxMoya", "Quick", "Nimble", "OHHTTPStubsSwift"]) // dev
+        .target(name: "RxMoya", dependencies: ["Moya", "RxSwift"])
     ]
 )
 
